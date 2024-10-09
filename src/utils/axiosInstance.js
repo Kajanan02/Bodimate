14277e7e2650ef71dcb363aaaa892444d8c296c4
Kajanan02 <Kajanan02000@gmail.com>
2024-10-09 22:16:52 +0530

    update url

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: `${import.meta.env.VITE_REACT_APP_HOST}/api`, // Ensure this matches your environment variable
+    baseURL: `${import.meta.env.VITE_REACT_APP_HOST}api`, // Ensure this matches your environment variable
     headers: {
         'Content-Type': 'application/json',
     },
