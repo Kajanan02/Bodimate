9a7a537ca9a68e831b61b56dbc5383c4922d41fc
Kajanan02 <Kajanan02000@gmail.com>
2024-10-12 01:07:46 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -27,8 +27,8 @@ import Loader from "./components/loader.jsx";
 import AdminSetting from "./pages/admin/setting/admin-Setting.jsx";
 import Analytics from "./pages/admin/analytics/analytics.jsx";
 import ConfirmationDialog from "./components/confirmation-dialog.jsx";
-import Register_Now from "./pages/ Bodimate-your-home/bodimte-register-now.jsx";
-import Owner_Register from "./pages/ Bodimate-your-home/bodimate-owner-register.jsx";
+import Register_Now from "./pages/Bodimate-your-home/bodimte-register-now.jsx";
+import Owner_Register from "./pages/Bodimate-your-home/bodimate-owner-register.jsx";
 
 
 
