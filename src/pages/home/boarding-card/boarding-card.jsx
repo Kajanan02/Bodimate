99bcfbd8d65e30d8d99ca45cd3e96c9e7dc04c1f
navaneethan077 <navaneethansivakumaran@gmail.com>
2024-10-24 23:21:06 +0530

    NearByUni, HomePage Update

--- src/pages/home/boarding-card/boarding-card.jsx
+++ src/pages/home/boarding-card/boarding-card.jsx
@@ -1,81 +1,76 @@
-import React, {useEffect, useState} from 'react';
-import bording from '../../../assets/boarding-details/description.jpg';
+import React, { useState } from 'react';
 import verifiedIcon from "../../../assets/boarding-details/verified.svg";
+import heartIcon from "../../../assets/home/heart.svg";
+import heartFilledIcon from "../../../assets/home/heart1.svg";
 import FeatherIcon from "feather-icons-react";
 import './boarding-card.css';
-import {useNavigate} from "react-router-dom";
-// import Heart from "react-heart";
-import axiosInstance from "../../../utils/axiosInstance.js";
-import {useDispatch} from "react-redux";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const BoardingCard = ({data, from = "",...props}) => {
-
+const BoardingCard = ({ data, from = "", ...props }) => {
     const navigate = useNavigate();
-    // console.log(data);
-    const dispatch = useDispatch();
-    const [active, setActive] = useState(false)
+    const [active, setActive] = useState(false);
 
     const handleClick = () => {
-        setActive(!active); //
+        setActive(!active);
     };
 
     return (
         <div>
-
-            <div className="card bodimate-card ">
+            <div className="card bodimate-card">
                 <div className="position-relative">
                     <div className={"position-absolute d-flex end-0 gap-2 bg-default"}>
-                        {from === "boardingOwner" ?
-                            <div className={"end-0 bg-default p-1"}><FeatherIcon icon={"edit"}
-                                                                                 className={"edit-icon cursor-pointer"}
-                                                                                 onClick={() => props.editItem()}/>
-                            </div> : ""}
-                        {from === "boardingOwner" ?
-                            <div className={"end-0 bg-default p-1"}><FeatherIcon icon={"trash-2"}
-                                                                                 className={"text-danger edit-icon cursor-pointer"}
-                                                                                 onClick={() => props.deleteItem()}/>
-                            </div> : ""}
+                        {from === "boardingOwner" && (
+                            <>
+                                <div className={"end-0 bg-default p-1"}>
+                                    <FeatherIcon
+                                        icon={"edit"}
+                                        className={"edit-icon cursor-pointer"}
+                                        onClick={() => props.editItem()}
+                                    />
+                                </div>
+                                <div className={"end-0 bg-default p-1"}>
+                                    <FeatherIcon
+                                        icon={"trash-2"}
+                                        className={"text-danger edit-icon cursor-pointer"}
+                                        onClick={() => props.deleteItem()}
+                                    />
+                                </div>
+                            </>
+                        )}
                     </div>
-                    <img src={data.boardingPic[0]} alt="Listing" className="img-fluid w-100 h-auto"/>
+                    <img src={data.boardingPic[0]} alt="Listing" className="img-fluid w-100 h-auto" />
                     <div className="heart-icon">
-                        <div
+                        <button
                             className="btn btn-heart ms-auto p-0"
-                            style={{
-                                width: "1.5rem",
-                                border: "none",
-                                outline: "none"
-                            }}
+                            onClick={handleClick}
+                            style={{ width: "1.5rem", border: "none", outline: "none", background: "transparent" }}
                         >
-                            {/*<Heart*/}
-                            {/*    isActive={active}*/}
-                            {/*    onClick={handleClick}*/}
-                            {/*    style={{*/}
-                            {/*        border: "none",*/}
-                            {/*        outline: "none"*/}
-                            {/*    }}*/}
-                            {/*/>*/}
-                        </div>
+                            <img
+                                src={active ? heartFilledIcon : heartIcon}
+                                alt="Heart Icon"
+                                className="heart-image"
+                            />
+                        </button>
                     </div>
                 </div>
                 <div className="p-3 cursor-pointer" onClick={() => navigate("/boarding-details/" + data._id)}>
                     <h6 className="review-rating p-0 m-0">
-                        <FeatherIcon icon={"star"}/> 4.8<span> (233 reviews)</span>
+                        <FeatherIcon icon={"star"} /> 4.8<span> (233 reviews)</span>
                     </h6>
                     <h5 className="d-flex align-items-center fs-6 p-0 m-0">
-                        {data.street},{data.district}
-
-                        {data.isVerified && <img src={verifiedIcon} alt="Verified Icon" className="verified-icon"/>}
+                        {data.street}, {data.district}
+                        {data.isVerified && <img src={verifiedIcon} alt="Verified Icon" className="verified-icon" />}
                     </h5>
-                    <p className="card-text p-0 m-0">{data.nearestUniversity}
-                        <br/>
+                    <p className="card-text p-0 m-0">
+                        {data.nearestUniversity}
+                        <br />
                     </p>
-                    <p className="price">{data.pricePerMonth} <span className="per-night">(Monthly)</span></p>
+                    <p className="price">
+                        {data.pricePerMonth} <span className="per-night">(Monthly)</span>
+                    </p>
                 </div>
             </div>
         </div>
-
-
     );
 };
 
